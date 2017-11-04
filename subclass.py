f3ecf4af429acfa7cabdafdc46a5032fd1100988
Matthew Giem <matthew.giem@gmail.com>
2017-11-04 16:55:06 -0700

    need to fix super() error:

--- subclass.py
+++ subclass.py
@@ -1,5 +1,12 @@
+import copy
 class ReversedStr(str):
     def __new__(*args, **kwargs):
         self = str.__new__(*args, **kwargs)
         self = self[::-1]
         return self
+
+class FilledList(list):
+    def __init__(self, count, value, *args, **kwargs):
+        super(FilledList, self).__init__(self, count, value, *args, **kwargs)
+        for _ in range(count):
+            self.append(copy.copy(value))
