f226d336b423699a6a43cec34520b41d12c53d2e
Matthew Giem <matthew.giem@gmail.com>
2017-11-20 12:37:33 -0800

    something wrong with radius.setter

--- circle.py
+++ circle.py
@@ -1,9 +1,21 @@
 class Circle:
-    def __init__(slef, diameter):
+    def __init__(self, diameter):
         self.diameter = diameter
 
     @property
     def radius(self):
         return self.diameter / 2
 
-small = circle(10)
+    @radius.setter
+    def radius(self, radius):
+        print("new setter")
+        self.diameter = radius * 2
+
+# small = Circle(10)
+# print("the diamter of the circle is {}".format(small.diameter))
+# print("the radius of the circle is {}".format(small.radius))
+# # change the radius of the Circle
+# small.radius = 20
+# print("we changed the radius to be 20")
+# print("the diameter of the circle is {}".format(small.diameter))
+# print("the radius of the circle is {}".format(small.radius))
