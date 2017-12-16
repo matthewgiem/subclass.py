7873015e5cffed443ea759cbc93ddd0e3baa0450
Matthew Giem <matthew.giem@gmail.com>
2017-12-15 23:07:12 -0800

    needs work on hand class

--- yatzy/test.py
+++ yatzy/test.py
@@ -1,8 +1,10 @@
 from hands import YatzyHand
 from dice import D6
 from dice import Die
+from hands import Hand
 
 die = Die(sides=5)
 print(die.value)
 D6 = D6()
 print(D6.value)
+hand = Hand(size=5, die_class=D6)
