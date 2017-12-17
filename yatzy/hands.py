ec66756c70fab52db192fa7c752316f33c126225
Matthew Giem <matthew.giem@gmail.com>
2017-12-16 23:43:43 -0800

    work on making sure dice are being added to the hand

--- yatzy/hands.py
+++ yatzy/hands.py
@@ -5,7 +5,7 @@ class Hand(list):
     def __init__(self, size=0, die_class=None, *args, **kwargs):
         if not die_class:
             raise ValueError("You must provide a die class")
-        super().__init__()
+        super(Hand, self).__init__()
 
         for _ in range(size):
             self.append(die_class())
--- yatzy/test.py
+++ yatzy/test.py
@@ -1,10 +1,10 @@
 from hands import YatzyHand
-from dice import D6
-from dice import Die
 from hands import Hand
+import dice
 
-die = Die(sides=5)
+die = dice.Die(sides=5)
 print(die.value)
-D6 = D6()
+D6 = dice.D6()
 print(D6.value)
-hand = Hand(size=5, die_class=D6)
+hand = Hand(size=5, die_class=dice.D6)
+print(hand)
